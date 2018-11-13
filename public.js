c975a0b48ce7737c3bbfa4a6f18fdcef140094e3
algernon1 <945341382@qq.com>
2018-11-13 13:54:12 +0800

    tj

--- public.js
+++ public.js
@@ -1840,7 +1840,7 @@ function limitCodeDeal(domArr,attrName){//codeArr：请求后台，返回code集
             isTrue = true;
         }
         if(!isTrue){//说明domArr集合中没有匹配到后台返回的codeArr集合中的权限整理，则此dom节点没有权限，进行remove操作
-            domArr.eq(nI).hide();
+            domArr.eq(nI).remove();
         }
     }
 
